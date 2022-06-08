8a4a6cf031525de2a1def96141cb9cd937e009fd
cenwadike <chinedunwadike04@gmailcom>
2022-06-08 18:46:16 +0100

    interaction scripts

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs"
+
 export interface networkConfigItem {
     ethUsdPriceFeed?: string
     blockConfirmations?: number
@@ -25,8 +27,8 @@ export const QUORUM_PERCENTAGE = 5 // Need 5% of voters to pass
 
 export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour before you can enact
 
-export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
-
 export const NEW_STORE_VALUE = 77
 export const FUNC = "store"
 export const PROPOSAL_DESCRIPTION = "Proposal #1 77 in the Box!"
+
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
---
+++ scripts/vote.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs"
+// @ts-ignore
+import { ethers, network } from "hardhat"
+import { moveBlocks } from "../utils/move-block"
+import { developmentChains, proposalsFile, VOTING_PERIOD } from "../helper-hardhat-config"
+
+const index = 0
+
+async function main(proposalIndex: number) {
+    const proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"))
+    // You could swap this out for the ID you want to use too
+    const proposalId = proposals[network.config.chainId!][proposalIndex]
+    // 0 = Against, 1 = For, 2 = Abstain for this example
+    const voteWay = 1
+    const reason = "I lika do da cha cha"
+    await vote(proposalId, voteWay, reason)
+}
+
+// 0 = Against, 1 = For, 2 = Abstain for this example
+export async function vote(proposalId: string, voteWay: number, reason: string) {
+    console.log("Voting...")
+    const governor = await ethers.getContract("GovernorContract")
+    const voteTx = await governor.castVoteWithReason(proposalId, voteWay, reason)
+    const voteTxReceipt = await voteTx.wait(1)
+    console.log(voteTxReceipt.events[0].args.reason)
+    const proposalState = await governor.state(proposalId)
+    console.log(`Current Proposal State: ${proposalState}`)
+    if (developmentChains.includes(network.name)) {
+        await moveBlocks(VOTING_PERIOD + 1)
+    }
+}
+
+main(index)
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
